b90cef860805f24cb373bc77b401709caf1d74c1
Dash <dkshowarth@gmail.com>
2023-08-04 16:04:05 -0700

    add functions for view actions, add prompts for "add" actions

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
 const db = require('mysql2');
 const inquirer = require('inquirer');
 
+const actionObject = {
+    'View All Departments': db.query(`SELECT * FROM departments`,  (err, results) => {
+        console.table(results)
+    }), 
+    'View All Roles': db.query(`SELECT * FROM roles`, (err, results) => {
+        console.table(results)
+    }), 
+    'View All Employees': db.query(`SELECT * FROM employeees`, (err, results) => {
+        console.table(results)
+    }),
+    'Add a Department': async () => {
+        const response = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Please enter the name of the new department:',
+                name: 'deptName',
+            }
+        ])
+    },
+    'Add a Role': async () => {
+        const response = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Please enter the name of the new role:',
+                name: 'roleName',
+            },
+            {
+                type: 'input',
+                message: 'Please enter the salary of the new role:',
+                name: 'roleName',
+            },
+            {
+                type: 'input',
+                message: 'Please select the department for this role:',
+                name: 'roleName',
+            }
+        ])
+    },
+    'Add an Employee': async () => {
+        const response = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Please enter the first name of the new employee:',
+                name: 'first_name',
+            },
+            {
+                type: 'input',
+                message: 'Please enter the last name of the new employee:',
+                name: 'last_name',
+            },
+            {
+                type: 'input',
+                message: 'Please enter the role of the new employee:',
+                name: 'role',
+            },
+            {
+                type: 'input',
+                message: "Please enter the name of the new employee's manager:",
+                name: 'manager',
+            }
+        ])
+    },
+    'Update an Employee Role': ,
+    'Exit' : exit()
+
+}
+
 const newUserAction = async function () {
     const userChoice = await inquirer.prompt([
         {
@@ -20,5 +87,4 @@ const newUserAction = async function () {
         }
     ])
 
-    // db.query(actionObject[userChoice.action])
 }
