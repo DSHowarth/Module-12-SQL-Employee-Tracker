cd15552a0f764f63ba48a9e3dca82d8067d462ca
Dash <dkshowarth@gmail.com>
2023-08-04 21:08:53 -0700

    comment js files

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ const newUserAction = async function () {
             ],
         }
     ])
+    //call function associated with the user's selection
     await actionObject[userChoice.action]();
+    //recursively call the prompt again to let them make another action.
+    //Weirdly, without this setTimeout function, the recursive prompt will
+    // appear above the console.table() and completely obliterate it when 
+    // the user tries to make another selection. I didn't have time to 
+    // dig into why, but this MacGyver'd solution works for now.
     setTimeout(() => {
         newUserAction();
     }, 1000);
